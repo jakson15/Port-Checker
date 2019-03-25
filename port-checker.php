edd20770a1e5d1f068c2ea0b3c54a93ce1cb154d
jakson15 <33206907+jakson15@users.noreply.github.com>
2019-03-26 00:09:07 +0100

    Fixed port 53 error

--- port-checker.php
+++ port-checker.php
@@ -14,25 +14,29 @@ function check_if_port_open( $ip, $transport, $port ) {
         $transport = 'tcp';
     }
 
+    $service_name = getservbyport( $port, $transport );
     $conection = fsockopen( $transport . '://' . $ip, $port, $errno, $errstr, $timeout = 3 );
     if ( $conection ){ 
             socket_set_timeout( $conection, $timeout );
-            $service_name = getservbyport( $port, $transport );
-            $write = fwrite($conection, "\x00");
+            if ( 'udp' != $transport ){
+                $write = fwrite($conection, "\x00");
+            }
             if ( ! $write ) {
                 echo "Nawiązano połącznie $transport z hostem. Port ($service_name) $port jest zamknięty. Błąd odpowiedzi portu.";
             }
             $startTime = time();
-            $header    = fread( $conection, 1 );
+            if ( 'udp' != $transport ){
+                $header = fread( $conection, 1 );
+            }
             $endTime   = time();
             $timeDiff  = $endTime - $startTime;
             if ( $timeDiff >= $timeout ) {
-                echo "Nawiązano połącznie $transport z hostem. Port ($service_name) $port jest otwarty";
+                echo "Nawiązano połącznie $transport z hostem. Port ($service_name) $port jest otwarty.";
                 fclose( $conection );
                 return 1;
             }
     } else {
-        echo "Port $port jest zamknięty. Wystąpił błąd nr. $errno: $errstr";
+        echo "Port ($service_name) $port jest zamknięty. Wystąpił błąd nr. $errno: $errstr";
         return false;
     }
 }
