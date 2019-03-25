b5250e0c185dd45203a894314977bd9ac5630345
jakson15 <33206907+jakson15@users.noreply.github.com>
2019-03-25 23:43:15 +0100

    Better error hendling

--- port-checker.php
+++ port-checker.php
@@ -20,14 +20,14 @@ function check_if_port_open( $ip, $transport, $port ) {
             $service_name = getservbyport( $port, $transport );
             $write = fwrite($conection, "\x00");
             if ( ! $write ) {
-                echo "Nawiązano połącznie z hostem. Port ($service_name) $port jest zamknięty. Błąd odpowiedzi portu.";
+                echo "Nawiązano połącznie $transport z hostem. Port ($service_name) $port jest zamknięty. Błąd odpowiedzi portu.";
             }
             $startTime = time();
             $header    = fread( $conection, 1 );
             $endTime   = time();
             $timeDiff  = $endTime - $startTime;
             if ( $timeDiff >= $timeout ) {
-                echo "Port ($service_name) $port jest otwarty";
+                echo "Nawiązano połącznie $transport z hostem. Port ($service_name) $port jest otwarty";
                 fclose( $conection );
                 return 1;
             }
@@ -36,4 +36,4 @@ function check_if_port_open( $ip, $transport, $port ) {
         return false;
     }
 }
-check_if_port_open( '79.133.211.124','udp', 5060 );
+check_if_port_open( '79.133.211.124','', 80 );
