8208c63f496482854a467f5cf4383a276dc4f0d6
jakson15 <33206907+jakson15@users.noreply.github.com>
2019-03-26 10:02:49 +0100

    Better structure

--- port-checker.php
+++ port-checker.php
@@ -8,26 +8,23 @@
  * @var string $port - number of port to check
  */
 function check_if_port_open( $ip, $transport, $port ) {
-    if ( 'udp' == strtolower($transport) || 'ssl' == strtolower($transport) || 'tls' == strtolower($transport)  ) {
+    if ( 'udp' == strtolower($transport) || 'ssl' == strtolower($transport) || 'tls' == strtolower($transport) ) {
         $transport = strtolower($transport);
     } else {
         $transport = 'tcp';
     }
 
     $service_name = getservbyport( $port, $transport );
-    $conection = fsockopen( $transport . '://' . $ip, $port, $errno, $errstr, $timeout = 3 );
-    if ( $conection ){ 
+    $conection    = fsockopen( $transport . '://' . $ip, $port, $errno, $errstr, $timeout = 1 );
+    if ( $conection ){
+        if ( 'udp' == $transport ){
             socket_set_timeout( $conection, $timeout );
-            if ( 'udp' != $transport ){
-                $write = fwrite($conection, "\x00");
-            }
+            $write = fwrite($conection, "\x00");
             if ( ! $write ) {
                 echo "Nawiązano połącznie $transport z hostem. Port ($service_name) $port jest zamknięty. Błąd odpowiedzi portu.";
             }
             $startTime = time();
-            if ( 'udp' != $transport ){
-                $header = fread( $conection, 1 );
-            }
+            $header    = fread( $conection, 1 );
             $endTime   = time();
             $timeDiff  = $endTime - $startTime;
             if ( $timeDiff >= $timeout ) {
@@ -35,9 +32,15 @@ function check_if_port_open( $ip, $transport, $port ) {
                 fclose( $conection );
                 return 1;
             }
+        } else {
+            echo "Nawiązano połącznie $transport z hostem. Port ($service_name) $port jest otwarty.";
+            fclose( $conection );
+            return 1;
+        }
+
     } else {
         echo "Port ($service_name) $port jest zamknięty. Wystąpił błąd nr. $errno: $errstr";
         return false;
     }
 }
-check_if_port_open( '79.133.211.124','', 80 );
+check_if_port_open( '79.133.211.124','udp', 5043 );
